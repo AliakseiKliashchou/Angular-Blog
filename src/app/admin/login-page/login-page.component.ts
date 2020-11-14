ae638483df8b375799fdb7d372d45a2f48c33488
HOME-PC\Алексей <iron-goga@yandex.ru>
2020-11-15 00:50:30 +0300

    pre auth

---
+++ src/app/admin/login-page/login-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/interfaces';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.minLength(6), Validators.required])
+    });
+  }
+
+  submit(): void {
+    if(this.form.invalid){
+      return;
+    };
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+  };
+
+}
